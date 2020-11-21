d914a1d78e0c683f4d1f1e02e7c484709b2caacd
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2020-11-21 15:49:21 -0500

    Positioning canvas

--- fractal.js
+++ fractal.js
@@ -1,7 +1,14 @@
+const canvasWidth = 600;
+const canvasHeight = 800;
+
 function setup() {
-  
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  background(200);
+  canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
 }
 
 function draw() {
-
+  colorMode(RGB);
+  fill(200,100,50);
+  circle(canvasWidth / 2,canvasHeight / 2, 100);
 }
