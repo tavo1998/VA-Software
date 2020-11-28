e0f508826cd1e8b9ab7a1c7d0567e503b8be5551
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2020-11-28 15:59:07 -0500

    added comments

--- fractal.js
+++ fractal.js
@@ -1,13 +1,18 @@
+//Tamaño del canvas
 const canvasWidth = 600;
 const canvasHeight = 700;
 
 let canvas;
+//Lineas a dibujar
 let lines = []
+//Cantidad de recursividad a realizar
 let count = 0;
 let slider;
+//Variables que manejan el centro de dibujo del fractal para poder desplazarlo.
 let translateX = 1;
 let translateY = 1;
 
+//Centrar canvas en la mitad de la pantalla
 function centerCanvas(){
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
@@ -17,6 +22,8 @@ function centerCanvas(){
   slider.style('width', '80px');
 }
 
+//Crear el canvvas con dimensiones canvasWidth, canvasHeight y color de fondo
+//Se crea las primeras lineas que forman el triangulo
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   background(200);
@@ -26,6 +33,7 @@ function setup() {
   lines.push(new Line(createVector(canvasWidth / 2, canvasHeight), createVector(canvasWidth, canvasHeight / 4), PI / 3));
 }
 
+//Función que dibuja en pantalla por cada frame
 function draw() {
   clear();
   colorMode(RGB);
@@ -39,10 +47,12 @@ function draw() {
   }
 }
 
+//Función que calcula la nueva posición del canvas cuando la ventana del navegador cambie de tamaño
 function windowResized() {
   centerCanvas();
 }
 
+//Función callback que lee las teclas que se presionan para poder mover el fractal.
 function keyPressed(){
   if(keyCode === UP_ARROW){
     translateY -= 10
@@ -52,7 +62,7 @@ function keyPressed(){
     translateX-= 10
   }else if(keyCode === RIGHT_ARROW){
     translateX+= 10
-  }else if(keyCode === 32 && count < 8){
+  }else if(keyCode === 32 && count < 7){
     let linesToDraw = [];
     for(lineToDivide of lines){
       linesToDraw = linesToDraw.concat(lineToDivide.generateChildrenLines());
@@ -62,6 +72,8 @@ function keyPressed(){
   }
 }
 
+//Clase que representa las lineas que son dibujadas y permite calcular las divisiones
+//donde la linea debe de cortarse para generar las nuevas
 class Line {
   constructor(startPoint, endPoint, degree){
     //Vectors
@@ -70,6 +82,7 @@ class Line {
     this.degree = degree;
   }
 
+  //Función que calcula las nuevas posiciones de las lineas para realizar la división (creación de las nuevas líneas)
   generateChildrenLines(){
     let childrenLines = [];
 
@@ -99,6 +112,7 @@ class Line {
     return childrenLines;
   }
 
+  //Función que dibuja una línea en el canvas, dependiendo de los puntos (vectores)
   drawLine(){
     line(this.startPoint.x, this.startPoint.y, this.endPoint.x, this.endPoint.y)
   }
