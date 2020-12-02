4a14d643adeea9852f7130cb644c92f22f0ee51e
tavo1998 <gustavo.pinto@correounivalle.edu.co>
2020-12-01 21:09:04 -0500

    zoom apply

--- main.js
+++ main.js
@@ -2,24 +2,28 @@
 const canvasWidth = 600;
 const canvasHeight = 700;
 
+
 let canvas;
 //Lineas a dibujar
 let lines = []
 //Cantidad de recursividad a realizar
 let count = 0;
-let slider;
 //Variables que manejan el centro de dibujo del fractal para poder desplazarlo.
 let translateX = 1;
 let translateY = 1;
+
+//Variable de escala
+let factorScale = 1;
+
+//Coordenadas mouse
+let coorMouseX;
+let coorMouseY;
 
 //Centrar canvas en la mitad de la pantalla
 function centerCanvas(){
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   canvas.position(x, y);
-  slider = createSlider(0,500,0,100);
-  slider.position(10, 300);
-  slider.style('width', '80px');
 }
 
 //Crear el canvvas con dimensiones canvasWidth, canvasHeight y color de fondo
@@ -28,6 +32,7 @@ function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   background(200);
   centerCanvas(canvas);
+  strokeWeight(0.3)
   lines.push(new Line(createVector(0 ,canvasHeight / 4), createVector(canvasWidth, canvasHeight / 4), -PI / 3));
   lines.push(new Line(createVector(0 ,canvasHeight / 4), createVector(canvasWidth / 2, canvasHeight), PI / 3));
   lines.push(new Line(createVector(canvasWidth / 2, canvasHeight), createVector(canvasWidth, canvasHeight / 4), PI / 3));
@@ -35,16 +40,25 @@ function setup() {
 
 //Función que dibuja en pantalla por cada frame
 function draw() {
+
+  coorMouseX = mouseX;
+  coorMouseY = mouseY;
+
+  //Trasladar la camara de acuerdo a las coordenadas del mouse
+  translate(coorMouseX, coorMouseY);
+  scale(factorScale);
+  translate(-coorMouseX, -coorMouseY);
+  translate();
+
   clear();
   colorMode(RGB);
   background(200);
   translate(translateX,translateY)
 
-  zoomFactor = slider.value();
-
   for(lineToDraw of lines){
     lineToDraw.drawLine();
   }
+
 }
 
 //Función que calcula la nueva posición del canvas cuando la ventana del navegador cambie de tamaño
@@ -55,13 +69,13 @@ function windowResized() {
 //Función callback que lee las teclas que se presionan para poder mover el fractal.
 function keyPressed(){
   if(keyCode === UP_ARROW){
-    translateY -= 10
+    translateY -= 15
   }else if(keyCode === DOWN_ARROW){
-    translateY += 10
+    translateY += 15
   }else if(keyCode === LEFT_ARROW){
-    translateX-= 10
+    translateX-= 15
   }else if(keyCode === RIGHT_ARROW){
-    translateX+= 10
+    translateX+= 15
   }else if(keyCode === 32 && count < 7){
     let linesToDraw = [];
     for(lineToDivide of lines){
@@ -71,6 +85,14 @@ function keyPressed(){
     count++;
   }
 }
+
+//Listener que escucha la rueda del mouse y cambia el factor de escala
+window.addEventListener("wheel", function(e) {
+  if (e.deltaY > 0)
+    factorScale *= 0.95;
+  else
+    factorScale *= 1.05;
+});
 
 
 
